4ac528e644995f40b29449e38da51a58c0a86e8b
Fayet Mathis <fmathis@localhost.localdomain>
2020-01-03 17:17:13 +0100

    Add my delivery include for .h

---
+++ include/get_next_line.h
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2019
+** Get_next_line.h
+** File description:
+** Epitech Project
+*/
+
+#ifndef GET_NEXT_LINE_H_
+#define GET_NEXT_LINE_H_
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#define READ_SIZE (4);
+
+#endif /* !GET_NEXT_LINE_H_ */
