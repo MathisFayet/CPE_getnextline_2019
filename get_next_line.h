ac3fe52dc503ac72208b037466d1ea3ce9371154
Fayet Mathis <fmathis@localhost.localdomain>
2020-01-04 16:15:02 +0100

    Add my .h for modification

--- get_next_line.h
+++ get_next_line.h
@@ -8,10 +8,6 @@
 #ifndef GET_NEXT_LINE_H_
 #define GET_NEXT_LINE_H_
 
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-
-#define READ_SIZE (4);
+#define READ_SIZE (4)
 
 #endif /* !GET_NEXT_LINE_H_ */
